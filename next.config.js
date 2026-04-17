e4b69bc43b8a2825e0ed3fc2a1f9c25e97242a65
Rohit <mahalam201@gmail.com>
2026-04-17 17:29:02 +0530

    Optimize: Add Vercel config and performance settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  poweredByHeader: false,
+  compress: true,
+  generateEtags: true,
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
